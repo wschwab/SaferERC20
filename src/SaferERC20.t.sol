f2454327ed0ea2b07c55f710fa0cc7fdb66edc85
wschwab <w.s.schwab4@gmail.com>
2021-12-05 13:36:56 +0200

    all test passing

--- src/SaferERC20.t.sol
+++ src/SaferERC20.t.sol
@@ -9,9 +9,7 @@ import "./mocks/MockSaferERC20.sol";
 import "./mocks/MockNonReceiver.sol";
 import "./mocks/GenericUser.sol";
 
-//TODO I think https://github.com/brockelmore/HEVMHelpers may solve my issues
-
-contract SaferERC2020Test is DSTest {
+contract SaferERC20Test is DSTest {
     MockERC20 token;
 
     function setUp() public {
@@ -27,30 +25,45 @@ contract SaferERC2020Test is DSTest {
     }
 
     function test_safeTransfer(address to, uint256 amount) public {
+        // exceptional cases
+        if(
+            to == address(0) || // transfer to zero address should fail
+            amount <= 2 // won't divide in two well, which messed things up
+        ) return;
+
+        uint256 halfAmount = amount / 2;
+
         // fund this contract so it can transfer
         token.mint(address(this), amount);
 
         // EOA
         uint256 preBal = token.balanceOf(to);
-        token.safeTransfer(to, amount);
+        token.safeTransfer(to, halfAmount);
         uint256 postBal = token.balanceOf(to);
 
-        assertEq(postBal - preBal, amount );
+        assertEq(postBal - preBal, halfAmount );
 
         // ERC20Receiver
-        token.mint(address(this), amount);
         ERC20Receiver recipient = new ERC20Receiver();
 
         preBal = token.balanceOf(address(recipient));
-        token.safeTransfer(address(recipient), amount);
+        token.safeTransfer(address(recipient), halfAmount);
         postBal = token.balanceOf(address(recipient));
 
-        assertEq(postBal - preBal, amount );
+        assertEq(postBal - preBal, halfAmount );
     }
 
     function test_safeTransferFrom(address to, uint256 amount) public {
+        // exceptional cases
+        if(
+            to == address(0) || // transfer to zero address should fail
+            amount <= 2 // won't divide in two well, which messed things up
+        ) return;
+
         // create "user"
         GenericUser from = new GenericUser();
+
+        uint256 halfAmount = amount / 2;
 
         // fund address
         token.mint(address(from), amount);
@@ -66,24 +79,24 @@ contract SaferERC2020Test is DSTest {
 
         // to EOA
         uint256 preBal = token.balanceOf(to);
-        token.safeTransferFrom(address(from), to, amount);
+        token.safeTransferFrom(address(from), to, halfAmount);
         uint256 postBal = token.balanceOf(to);
 
-        assertEq(postBal - preBal, amount);
+        assertEq(postBal - preBal, halfAmount);
 
         // to ERC20Receiver
-        token.mint(address(from), amount);
+        // token.mint(address(from), amount);
         ERC20Receiver recipient = new ERC20Receiver();
 
         preBal = token.balanceOf(address(recipient));
-        token.safeTransferFrom(address(from), address(recipient), amount);
+        token.safeTransferFrom(address(from), address(recipient), halfAmount);
         postBal = token.balanceOf(address(recipient));
 
-        assertEq(postBal - preBal, amount );
+        assertEq(postBal - preBal, halfAmount );
     }
 
     // for non ERC20Receiver
-    function testFail_safeTransfer(address to, uint256 amount) public {
+    function testFail_safeTransfer(uint256 amount) public {
         ERC20 nonReceiver = new ERC20("non", "NON");
         token.mint(address(this), amount);
 
@@ -91,7 +104,7 @@ contract SaferERC2020Test is DSTest {
         token.safeTransfer(address(nonReceiver), amount);
     }
     // for non ERC20Receiver
-    function testFail_safeTransferFrom(address to, uint256 amount) public {
+    function testFail_safeTransferFrom(uint256 amount) public {
         ERC20 nonReceiver = new ERC20("non", "NON");
         GenericUser from = new GenericUser();
 
